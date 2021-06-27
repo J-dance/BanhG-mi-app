e4073b73cd2efc9a0f338e2b1ce079d672e23e21
J-dance <dance.jojo@gmail.com>
2021-06-27 13:18:46 +0100

    confirmation page complete

--- banhg-mi-app/src/app/confirmation/confirmation.component.ts
+++ banhg-mi-app/src/app/confirmation/confirmation.component.ts
@@ -1,53 +1,37 @@
 import { Component, OnInit, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-} from '@angular/animations';
+
+
+
+@NgModule({
+  imports: [ 
+    BrowserModule 
+  ]
+})
 
 @Component({
   selector: 'app-confirmation',
   templateUrl: './confirmation.component.html',
   styleUrls: ['./confirmation.component.scss'],
-  animations: [
-    // animation triggers go here
-  ]
 })
-
-@NgModule({
-    imports: [ 
-      BrowserAnimationsModule,
-      BrowserModule 
-    ]
-  })
 
 export class ConfirmationComponent implements OnInit {
 
   isLoading: boolean = true;
+  
+
+  
 
   constructor() { }
 
+
   ngOnInit(): void {
-    setTimeout(this.Loaded, 3000)
+    setTimeout(this.Loaded, 7000);
   }
 
   Loaded = ():void => {
     this.isLoading = false;
-    alert('Order Complete!');
+    alert('Thank you');
   }
 
 }
-
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-// @NgModule({
-//   imports:      [ BrowserAnimationsModule ],
-//   declarations: [ ... ],
-//   bootstrap:    [ ... ]
-// })
-
-// export class AppModule { }
